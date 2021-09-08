4d31d67e6531663e77e0ee2fa2d11060fa719df1
Darren Picard <darren.picard@neofinancial.com>
2021-09-07 21:33:26 -0600

    Setup new route for api-gateway

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
-
+  # 
   assume_role_policy = jsonencode({
     Version : "2012-10-17",
     Statement : [
@@ -20,48 +20,49 @@ resource "aws_iam_role" "iam_for_lambda" {
     ]
   })
 }
-
+# 
 resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.iam_for_lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
+# 
 resource "aws_cloudwatch_log_group" "hello_world" {
   name              = "/aws/lambda/${aws_lambda_function.get_brew_lambda.function_name}"
   retention_in_days = 14
 }
-
+# 
 data "archive_file" "lambda_zip_file" {
   type        = "zip"
-  source_file = "${path.module}/../target/release/bootstrap"
+  source_file = "${path.module}/../brew_book_brew_api/target/release/bootstrap"
   output_path = "${path.module}/../lambda.zip"
 }
-
+# 
 resource "aws_lambda_function" "get_brew_lambda" {
   filename      = data.archive_file.lambda_zip_file.output_path
   function_name = "brew_book_get_api"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "hello.handler"
-
+  # 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
   source_code_hash = filebase64sha256(data.archive_file.lambda_zip_file.output_path)
-
+  # 
   runtime = "provided.al2"
 }
-
+# 
 resource "aws_apigatewayv2_api" "brew_book_apigateway" {
-  name          = "brew_book_apigateway"
-  protocol_type = "HTTP"
+  name                         = "brew_book_apigateway"
+  protocol_type                = "HTTP"
+  disable_execute_api_endpoint = false
 }
-
+# 
 resource "aws_apigatewayv2_stage" "apigateway_stage" {
   api_id = aws_apigatewayv2_api.brew_book_apigateway.id
-
+  # 
   name        = "ApiGateway_stage"
   auto_deploy = true
-
+  # 
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.api_gw.arn
     format = jsonencode({
@@ -78,6 +79,13 @@ resource "aws_apigatewayv2_stage" "apigateway_stage" {
     })
   }
 }
+# 
+resource "aws_apigatewayv2_integration" "get_brew_integration" {
+  api_id             = aws_apigatewayv2_api.brew_book_apigateway.id
+  integration_uri    = aws_lambda_function.get_brew_lambda.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+}
 
 resource "aws_apigatewayv2_integration" "post_brew_integration" {
   api_id             = aws_apigatewayv2_api.brew_book_apigateway.id
@@ -85,13 +93,19 @@ resource "aws_apigatewayv2_integration" "post_brew_integration" {
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
 }
-
-resource "aws_apigatewayv2_route" "hello_world" {
+# 
+resource "aws_apigatewayv2_route" "get_brew" {
   api_id    = aws_apigatewayv2_api.brew_book_apigateway.id
   route_key = "GET /brew"
-  target    = "integrations/${aws_apigatewayv2_integration.post_brew_integration.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.get_brew_integration.id}"
 }
 
+resource "aws_apigatewayv2_route" "post_brew" {
+  api_id    = aws_apigatewayv2_api.brew_book_apigateway.id
+  route_key = "POST /brew"
+  target    = "integrations/${aws_apigatewayv2_integration.post_brew_integration.id}"
+}
+# 
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -99,18 +113,19 @@ resource "aws_lambda_permission" "api_gw" {
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.brew_book_apigateway.execution_arn}/*/*"
 }
-
+# 
 resource "aws_cloudwatch_log_group" "api_gw" {
   name              = "/aws/api_gw/${aws_apigatewayv2_api.brew_book_apigateway.name}"
   retention_in_days = 14
 }
-
+# 
 output "function_name" {
   description = "Name of the Lambda function."
   value       = aws_lambda_function.get_brew_lambda.function_name
 }
-
+# 
 output "base_url" {
   description = "Base URL for API Gateway stage."
   value       = aws_apigatewayv2_stage.apigateway_stage.invoke_url
 }
+# 
---
+++ s3.tf
@@ -0,0 +1,30 @@
+# resource "aws_s3_bucket" "home_server_lambda_zips" {
+#   bucket = "home-server-lambda-zips"
+#   acl    = "private"
+
+#   tags = {
+#     Name = "Home Server lambda Zips"
+#   }
+# }
+
+# resource "aws_s3_bucket" "home_server_statics" {
+#   bucket = "home-server-statics"
+#   acl    = "public-read"
+#   policy = jsonencode({
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Sid" : "PublicRead",
+#         "Effect" : "Allow",
+#         "Principal" : "*",
+#         "Action" : ["s3:GetObject", "s3:GetObjectVersion"],
+#         "Resource" : ["arn:aws:s3:::home-server-statics/*"]
+#       }
+#     ]
+#   })
+
+#   website {
+#     index_document = "index.html"
+#     error_document = "error.html"
+#   }
+# }
